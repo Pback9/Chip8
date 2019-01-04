f5b731606bec946491e7b8023752f245fec38ad7
Parker Back <pback9@gmail.com>
2019-01-04 16:52:56 -0500

    Temporary fix for key input issues.

--- Chip8/Source.cpp
+++ Chip8/Source.cpp
@@ -13,33 +13,42 @@ int modified_width = DISPLAY_WIDTH * resize_modifier;
 
 chip8 emuChip8;
 
-void updateKeyboard(chip8 emuChip8);
+//void updateKeyboard(chip8 emuChip8);
 
 int main(int argc, char **argv) {
-
+	
 	// check to see if the rom loaded properly
 	if (!emuChip8.loadGame("pong.ch8"))
 		return 1;
 
 	// create a render window and set it's background color to black
 	sf::RenderWindow window(sf::VideoMode(modified_width, modified_height), "Chip8");
-	window.setFramerateLimit(60);
+	window.setFramerateLimit(180);
 	window.clear();
 
 	// emulation loop loop
 	while (window.isOpen()) {
-		sf::Event event;
 
-		while (window.pollEvent(event)) {
-			if (event.type = sf::Event::Closed)
-				window.close();
+		//updateKeyboard(emuChip8);
+		emuChip8.key[0x1] = sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Num1);
+		emuChip8.key[0x2] = sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Num2);
+		emuChip8.key[0x3] = sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Num3);
+		emuChip8.key[0xC] = sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Num4);
 
-			// exit if escape is pressed
-			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape)
-				window.close();
-		}
+		emuChip8.key[0x4] = sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Q);
+		emuChip8.key[0x5] = sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W);
+		emuChip8.key[0x6] = sf::Keyboard::isKeyPressed(sf::Keyboard::Key::E);
+		emuChip8.key[0xD] = sf::Keyboard::isKeyPressed(sf::Keyboard::Key::R);
 
-		updateKeyboard(emuChip8);
+		emuChip8.key[0x7] = sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A);
+		emuChip8.key[0x8] = sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S);
+		emuChip8.key[0x9] = sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D);
+		emuChip8.key[0xE] = sf::Keyboard::isKeyPressed(sf::Keyboard::Key::F);
+
+		emuChip8.key[0xA] = sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Z);
+		emuChip8.key[0x0] = sf::Keyboard::isKeyPressed(sf::Keyboard::Key::X);
+		emuChip8.key[0xB] = sf::Keyboard::isKeyPressed(sf::Keyboard::Key::C);
+		emuChip8.key[0xF] = sf::Keyboard::isKeyPressed(sf::Keyboard::Key::V);
 		emuChip8.emulateCycle();
 
 		if (emuChip8.draw_flag == true) {
@@ -60,25 +69,7 @@ int main(int argc, char **argv) {
 	}
 }
 
-void updateKeyboard(chip8 emuChip8) {
-	emuChip8.key[0x1] = sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Num1);
-	emuChip8.key[0x2] = sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Num2);
-	emuChip8.key[0x3] = sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Num3);
-	emuChip8.key[0xC] = sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Num4);
+//void updateKeyboard(chip8 emuChip8, sf::RenderWindow window) {
+	
 
-	emuChip8.key[0x4] = sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Q);
-	emuChip8.key[0x5] = sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W);
-	emuChip8.key[0x6] = sf::Keyboard::isKeyPressed(sf::Keyboard::Key::E);
-	emuChip8.key[0xD] = sf::Keyboard::isKeyPressed(sf::Keyboard::Key::R);
-
-	emuChip8.key[0x7] = sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A);
-	emuChip8.key[0x8] = sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S);
-	emuChip8.key[0x9] = sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D);
-	emuChip8.key[0xE] = sf::Keyboard::isKeyPressed(sf::Keyboard::Key::F);
-
-	emuChip8.key[0xA] = sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Z);
-	emuChip8.key[0x0] = sf::Keyboard::isKeyPressed(sf::Keyboard::Key::X);
-	emuChip8.key[0xB] = sf::Keyboard::isKeyPressed(sf::Keyboard::Key::C);
-	emuChip8.key[0xF] = sf::Keyboard::isKeyPressed(sf::Keyboard::Key::V);
-
-}
+//}
